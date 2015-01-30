2f05da76daabb70d4751dd05ee4370746a0ce390
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2015-01-30 14:31:11 -0500

    adds names properly

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http)
+var io = require('socket.io')(http),
+    name = "";
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
+  socket.on('name', function(data) {
+      socket.name = data;
+      io.sockets.emit('name', name);
+  });
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', { name: socket.name, data: msg });
   });
 
 });http.listen(process.env.PORT || 3000, function(){
